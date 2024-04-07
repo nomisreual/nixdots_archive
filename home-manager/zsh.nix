35e3c87b562b57079f28bfb10489b810e5759f6d
Simon Antonius Lauer <simon.lauer@posteo.de>
2024-04-07 22:33:57 +0200

    feat: added zsh shell aliases

--- home-manager/zsh.nix
+++ home-manager/zsh.nix
@@ -15,5 +15,10 @@
       path=('/home/simon/.local/bin' $path)
       export PATH
     '';
+    shellAliases = {
+      py = "nix develop github:nomisreual/DevShells -c zsh";
+      py310 = "nix develop github:nomisreual/DevShells#py310 -c zsh";
+      py312 = "nix develop github:nomisreual/DevShells#py312 -c zsh";
+    };
   };
 }
